975e22dfe6f5acb13b45fc2249f0616f49dd0f6a
Hemendra Lalawat <hemendralalawat30@gmail.com>
2023-03-19 20:52:15 +0530

    changes in data-source file

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,12 +5,27 @@ import { Messages } from "./entity/Message"
 import { Plan } from "./entity/Plans"
 import { User, Token, Subscribers } from "./entity/User"
 
+// export const AppDataSource = new DataSource({
+//     type: "mysql",
+//     host: "localhost",
+//     port: 3306,
+//     username: "root",
+//     password: "",
+//     database: "riders",
+//     synchronize: true,
+//     logging: false,
+//     entities: [User, Token, Messages, Request, Friends, Group, Subscribers, Plan],
+//     migrations: [],
+//     subscribers: [],
+// })
+
+
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "root",
-    password: "",
+    username: "riders",
+    password: "zGRHCAJWwVoc7r3KrGAq",
     database: "riders",
     synchronize: true,
     logging: false,
