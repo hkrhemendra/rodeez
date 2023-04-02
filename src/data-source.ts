8417caf98adf97828810297043b8df88e29cd5f0
hkrhemendra <51264979+hkrhemendra@users.noreply.github.com>
2023-04-02 14:39:27 +0530

    Changess

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "127.0.0.1",
     port: 3306,
     username: "riders",
     password: "zGRHCAJWwVoc7r3KrGAq",
